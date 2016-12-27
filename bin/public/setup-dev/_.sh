07c92ae0afc5b2e3953c564e23fe796a8c61260b
da99 <i-hate-spam-1234567@mailinator.com>
2016-12-27 14:00:08 -0600

    Added: trepl dep.

--- bin/public/setup-dev/_.sh
+++ bin/public/setup-dev/_.sh
@@ -2,4 +2,5 @@
 # === {{CMD}}  # Setup machine for development.
 setup-dev () {
   lua_setup install luajit luarocks
+  $0 luarocks install --server=http://luarocks.org/dev trepl-torchless
 } # === end function
